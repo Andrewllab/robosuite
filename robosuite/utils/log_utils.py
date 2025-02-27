13903862a33df0d76a7abda27f7abf7ec7c67cd7
Andrewllab <82389649+Andrewllab@users.noreply.github.com>
2025-02-27 14:07:23 +0100

    Update log_utils.py
    adding os import

--- robosuite/utils/log_utils.py
+++ robosuite/utils/log_utils.py
@@ -3,6 +3,7 @@ This file contains utility classes and functions for logging to stdout and stder
 Adapted from robomimic: https://github.com/ARISE-Initiative/robomimic/blob/master/robomimic/utils/log_utils.py
 """
 import logging
+import os
 
 from termcolor import colored
 
